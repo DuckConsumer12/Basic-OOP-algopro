b3a248f886daba502128ef80e5012c8025429f22
NaJK4cK <144893954+DuckConsumer12@users.noreply.github.com>
2023-12-04 10:10:29 +0700

    Add files via upload

---
+++ exercisemodule.py
@@ -0,0 +1,8 @@
+import programexercise
+
+class ExerciseDriverJS:
+    def __init__(self, n, c):
+        self.__name = n
+        self.__content = c
+
+    
---
+++ programexercise.py
@@ -0,0 +1,28 @@
+class ProgramExercise:
+    def __init__(self, n, p):
+        self.__name = n
+        self.__price = p
+
+
+    
+    def thecontent(self):
+        return list_items 
+    
+    def setPriceJS(self):
+        self.__price
+
+    def setContent(self):
+        input_item = input("Enter item: ")
+        list_items.append(input_item)
+        
+        list_items = {
+            "Dry cured Iberian Ham" : "$177.80",
+            "Wagyu steaks" : "$450.00",
+            "Matsutake Mushrooms" : "$272.00",
+            "Kopi Luwak Coffee" : "$306.50",
+            "Moose Cheese" : "$487.20",
+            "White Truffles" : "$3600.00",
+            "Blue Fin Tuna" : "$3603.00",
+            "Le Bonnotte Potatoes" : "$270.81"
+        }
+
